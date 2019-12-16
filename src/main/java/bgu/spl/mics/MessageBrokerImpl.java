9810b2ae13f18f6d8cbdf5921d7e39721aac8f40
Naama-Le <naamalevy25@gmail.com>
2019-12-16 21:51:14 +0200

    Changes in MessageBrokerImpl - Hillel

--- src/main/java/bgu/spl/mics/MessageBrokerImpl.java
+++ src/main/java/bgu/spl/mics/MessageBrokerImpl.java
@@ -1,17 +1,28 @@
 package bgu.spl.mics;
 
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+
 
 /**
  * The {@link MessageBrokerImpl class is the implementation of the MessageBroker interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
-public class MessageBrokerImpl implements MessageBroker {
-	private ConcurrentHashMap<Subscriber, BlockingQueue<Message>> messageQueuesMap;
 
-	// creating a singleton
+public class MessageBrokerImpl implements MessageBroker {
+
+	//***  fields  ***
+	private ConcurrentHashMap<Subscriber, BlockingQueue<Message>> subscriberMessagesQueuesMap; //holds subscribers and their messages queue
+	private ConcurrentHashMap<Class<? extends Message>, Queue<Subscriber>> messageSubscribersQueueMap; // holds messages and subscribed subscribers queue
+	private ConcurrentHashMap<Event, Future> eventFutureMap; // holds Events and their Future
+	//   private final Object lock;  // QQQ need to understand what it does
+
+
+
+
+	//***  constructor (singleton, thread-safe)  ***
 	private static class SingletonHolder {
 		private static MessageBrokerImpl messageBroker = new MessageBrokerImpl();
 	}
@@ -23,17 +34,25 @@ public class MessageBrokerImpl implements MessageBroker {
 		return SingletonHolder.messageBroker;
 	}
 
-	private MessageBrokerImpl(){
-		messageQueuesMap = new ConcurrentHashMap<>();
+	private MessageBrokerImpl(){  // initializes fields
+		messageSubscribersQueueMap = new ConcurrentHashMap<>();
+		subscriberMessagesQueuesMap = new ConcurrentHashMap<>();
+		eventFutureMap = new ConcurrentHashMap<>();
+		//    lock = new Object(); // QQQ need to understand what it does
+
 	}
+
 	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber m) {
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber s) {
+
+
+
+
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public void subscribeBroadcast(Class<? extends Broadcast> type, Subscriber m) {
+	public void subscribeBroadcast(Class<? extends Broadcast> type, Subscriber s) {
 		// TODO Auto-generated method stub
 
 	}
@@ -58,20 +77,20 @@ public class MessageBrokerImpl implements MessageBroker {
 	}
 
 	@Override
-	public void register(Subscriber m) {
+	public void register(Subscriber s) {
 /*		BlockingQueue<Message> subQueue;
 		messageQueuesMap.put(m, subQueue);*/
 
 	}
 
 	@Override
-	public void unregister(Subscriber m) {
+	public void unregister(Subscriber s) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public Message awaitMessage(Subscriber m) throws InterruptedException {
+	public Message awaitMessage(Subscriber s) throws InterruptedException {
 		// TODO Auto-generated method stub
 		return null;
 	}
